eec9b21cd5477a5700de27f28c52523952a2c1ea
fredb <fred.brosman06@gmail.com>
2025-02-07 21:01:14 +0200

    createContext and using context

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import Counter from "./components/Counter.jsx";
 import BasicEffect from "./components/BasicEffect.jsx";
 import CounterEffect from "./components/CounterEffect.jsx";
 import FetchDataEffect from "./components/FetchDataEffect.jsx";
+import UserProfile from "./components/UserProfile.jsx";
+import {UserProvider} from "./components/UserContext.jsx";
+import UpdateUser from "./components/UpdateUser.jsx";
 
 const App = () => {
     return (
-        <div>
-            <BasicEffect/>
-            <CounterEffect />
-            <FetchDataEffect/>
-        </div>
+        <UserProvider>
+
+            <UserProfile/>
+            <UpdateUser/>
+        </UserProvider>
     );
 }
 
